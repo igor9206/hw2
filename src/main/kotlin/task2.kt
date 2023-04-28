b17c5dced72b3c14ef07c2e52027ca7471dd41a8
Igor Korshunov <igor.9206@gmail.com>
2023-04-28 16:07:11 +0600

    add task2

---
+++ src/main/kotlin/task2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val likes = 111
+
+    if (likes % 10 == 1 && likes % 100 != 11) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+
+}
