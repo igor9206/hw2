9c5d21eeedec8eceeffbc081ca1cb29f46f909ec
Igor Korshunov <igor.9206@gmail.com>
2023-04-28 17:29:59 +0600

    add task3

---
+++ src/main/kotlin/task3.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val buy = 101
+    val regularCustomer = true
+
+    if (buy > 0 && buy <= 1000) {
+        println("покупка — $buy руб. →")
+        if (regularCustomer) regularDiscount(buy)
+    } else if (buy > 1000 && buy <= 10_000) {
+        println("покупка — $buy руб. →")
+        val afterDiscount = discount100Rub(buy)
+        if (regularCustomer) regularDiscount(afterDiscount)
+    } else if (buy > 10_000) {
+        println("покупка — $buy руб. →")
+        val afterDiscount = discount5Percent(buy)
+        if (regularCustomer) regularDiscount(afterDiscount)
+    }
+}
+
+fun regularDiscount(buy: Int): Int {
+    val result = buy - (buy * 1 / 100)
+    println("после применения 1% скидки — $result рубля.")
+    return result
+}
+
+fun discount100Rub(buy: Int): Int {
+    val result = buy - 100
+    println("после применения скидки — $result  руб.")
+    return result
+}
+
+fun discount5Percent(buy: Int): Int {
+    val result = buy * 95 / 100
+    println("после применения скидки 5% — $result руб.")
+    return result
+}
